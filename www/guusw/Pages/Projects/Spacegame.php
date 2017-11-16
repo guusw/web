4e59b4bd2a34bfccc7dd43e868dec2cb27ccd344
guusw <guus_waals@live.nl>
2017-11-16 20:03:57 +0100

    Update spacegame text

--- www/guusw/Pages/Projects/Spacegame.php
+++ www/guusw/Pages/Projects/Spacegame.php
@@ -13,7 +13,7 @@ The rendering pipeline I wrote for this project uses OpenGL, has HDR rendering a
 I also wrote the GUI system used throughout the game and the menu, it is a basic combination of horizontal and vertical layout containers.
 </p>
 <p>
-For the networking side, I wrote a basic encryption layer for the packet using RSA as a key exchange system and AES for all the game packets. I wrote this system from scratch to figure out the implementations of public key cryptography and symmetric key cryptography.
+For the networking side, I wrote a basic encryption layer for network traffic that uses AES encryption and RSA for exchanging keys. I wrote this system from scratch in order to learn more public key cryptography and symmetric key cryptography.
 </p>
 
 <?php $gallery_path = "Videos/spacegame.webm,Images/Spacegame"; require "Gallery.php"; ?>
